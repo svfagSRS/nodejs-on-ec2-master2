1254cb8eff6a3c38547aaa024355ecdd15681f20
svfagSRS <161646457+svfagSRS@users.noreply.github.com>
2024-05-29 21:39:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('You are in AWS Cloud. This is TECHESSAY. Please share, subscribe, and like my videos.'); // Write a response to the client
   res.end(); // End the response
-}).listen(5000); // The server object listens on port 5000
+}).listen(4000); // The server object listens on port 4000
